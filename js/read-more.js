470b795ade76c4fdb7e9f139a884e6569d70ee8b
npolizotis <npolizotis@gmail.com>
2016-07-20 11:31:21 +0300

    Create read-more.js

---
+++ js/read-more.js
@@ -0,0 +1,22 @@
+$(function(){
+
+//add css
+// should be added at PB level 
+ $('head').append('<link rel="stylesheet" href="https://gitcdn.link/repo/npolizotis/dummy-cdn/master/css/read-more.css" type="text/css" />');
+
+
+//before 
+$('<input type="checkbox" class="read-more-state" id="post-2">').insertBefore(".articleMeta");
+
+  
+//after 
+$('<label for="post-2" class="read-more-trigger"></label>').insertAfter(".articleMeta");
+
+//Read more targets
+$(".authorNotes,.publicationContentDoi,.publicationContentReceivedDate.dates ,.publicationContentAcceptedDate.dates,.publicationContentEpubDate.dates").addClass("read-more-target");
+
+// pb configuration replaceable
+ $(".general-image.leaderBoard,#3bbadb9a-40c6-420c-b5b4-85856e392b06,#763eb2fa-1c80-480e-9667-29e9233f2ca3").addClass("hidden");
+$(".articleMeta").addClass("read-more-wrap");
+
+});
