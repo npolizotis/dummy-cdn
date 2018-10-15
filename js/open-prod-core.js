4f19557549929e563b690e0b52ecba87665d5d96
Nikos Polyzotis <>
2018-10-15 17:32:58 +0300

    moved everything to prod-core

--- js/open-prod-core.js
+++ js/open-prod-core.js
@@ -1,38 +1,47 @@
-(function($){
+var jQueryVersion = '2.2.4'
 
-	$(function(){
-		$('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn/css/lightbox.css" type="text/css" />');
-		$.get("https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn//html/product.html",function(data){
-				//laod html
-				$("body").append(data);
-				
+if (window.jQuery === undefined || window.jQuery.fn.jquery < jQueryVersion) {
+  var done = false
+  var script = document.createElement('script')
+  script.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jQueryVersion + '/jquery.min.js'
+  script.onload = script.onreadystatechange = function () {
+    if (!done && (!this.readyState || this.readyState === 'loaded' || this.readyState === 'complete')) {
+      done = true
+      initMyBookmarklet()
+    }
+  }
+  document.getElementsByTagName('head')[0].appendChild(script)
+} else {
+  initMyBookmarklet()
+}
 
-				 
-            
-            var close=function (e){
-                if(e.keyCode===27){
-                    $("a.closemsg")[0].click();
-                    $("#product-inner").addClass("done");
-                }
-            }
-            $(document).on('keyup','#product-inner:not(.done)',close);   
-            
-            $(".lightbox a.hidden")[0].click();
-            $(".lightbox input")[0].focus();
-            $("#product-inner form").on("submit", function(e) {
-                e.preventDefault();
-                if ($("#product-inner.lightbox form")[0].checkValidity()) 
-                {
-                    var product = $("#product-inner input#product").val();
-                    var environment = $("#product-inner input:radio[name=environment]:checked").val();
-                    window.open("http://" + product + "-" + environment + ".literatumonline.com");
-                    return false;
-                }
-            });
-        
+function initMyBookmarklet () {
+  (function ($) {
+    $(function () {
+      $('head').append('<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn/css/lightbox.css" type="text/css" />')
+      $.get('https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn/html/product.html', function (data) {
+        // laod html
+        $('body').append(data)
+        var close = function (e) {
+          if (e.keyCode === 27) {
+            $('a.closemsg')[0].click()
+            $('#product-inner').addClass('done')
+          }
+        }
+        $(document).on('keyup', '#product-inner:not(.done)', close)
 
-			})
-
-	});
-
-})(window.jQuery);
+        $('.lightbox a.hidden')[0].click()
+        $('.lightbox input')[0].focus()
+        $('#product-inner form').on('submit', function (e) {
+          e.preventDefault()
+          if ($('#product-inner.lightbox form')[0].checkValidity()) {
+            var product = $('#product-inner input#product').val()
+            var environment = $('#product-inner input:radio[name=environment]:checked').val()
+            window.open('http://' + product + '-' + environment + '.literatumonline.com')
+            return false
+          }
+        })
+      })
+    })
+  })(window.jQuery)
+}
--- js/open-prod.js
+++ js/open-prod.js
@@ -1,26 +1,5 @@
-(function(){
-
-	// the minimum version of jQuery we want
-	var v = "2.2.4";
-
-	// check prior inclusion and version
-	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "//ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initMyBookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		initMyBookmarklet();
-	}
-	
-	function initMyBookmarklet() {
-		$.getScript('https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn/js/open-prod-core.min.js');
-	}
-
-})();
+(function () {
+  var script = document.createElement('script')
+  script.src = 'https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn/js/open-prod-core.min.js'
+  document.getElementsByTagName('head')[0].appendChild(script)
+}())
