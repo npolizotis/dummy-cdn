2bc0fb80dca1e4b28d985c22e1781366df254ef3
Nikos Polyzotis <>
2018-10-15 17:37:06 +0300

    hardcoded version

--- js/open-prod.js
+++ js/open-prod.js
@@ -1,5 +1,5 @@
 (function () {
   var script = document.createElement('script')
-  script.src = 'https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn@v1.2.0/js/open-prod-core.min.js'
+  script.src = 'https://cdn.jsdelivr.net/gh/npolizotis/dummy-cdn@1.2.0/js/open-prod-core.min.js'
   document.getElementsByTagName('head')[0].appendChild(script)
 }())
